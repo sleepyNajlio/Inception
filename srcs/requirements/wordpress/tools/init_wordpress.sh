a2972adad0d3aa6907d141868df163d3c697c2b9
najlio <najlio@najlios-MacBook-Pro.local>
2023-09-14 23:28:25 +0100

    pffff

--- srcs/requirements/wordpress/tools/init_wordpress.sh
+++ srcs/requirements/wordpress/tools/init_wordpress.sh
@@ -3,7 +3,9 @@ if [ ! -f /var/www/html/wp-config.php ]; then
     cd /var/www/html
 
     wp core download --allow-root;
-    mv /var/www/wp-config.php /var/www/html/
+    cp /tmp/wp-config.php /var/www/html/
+
+    # sleep 10
     # wp config create  --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_USER_PASSWORD --dbhost=$DB_HOST --allow-root
 
     wp core install --url='https://nloutfi.42.fr' --title="My WordPress Site" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
